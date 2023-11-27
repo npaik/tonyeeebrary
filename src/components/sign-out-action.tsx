0a4a5bc81100864e86992480c9f469ede01721f7
npaik <nakhyunpaik@gmail.com>
2023-11-27 11:39:34 -0800

    redirect

--- src/components/sign-out-action.tsx
+++ src/components/sign-out-action.tsx
@@ -1,11 +1,13 @@
-import { signOut } from "@/auth"
-import SignoutButton from "@/components/sign-out-button"
+import { signOut } from "@/auth";
+import SignoutButton from "@/components/sign-out-button";
 
 export default function SignOutAction() {
   const handleSignOut = async () => {
-    "use server"
-    await signOut({ redirectTo: "/api/auth/signin?callbackUrl=http://localhost:3000/" })
-  }
+    "use server";
+    await signOut({
+      redirectTo: "/api/auth/signin?callbackUrl=tonyeeebrary.vercel.app",
+    });
+  };
 
-  return <SignoutButton signOut={handleSignOut} />
+  return <SignoutButton signOut={handleSignOut} />;
 }
