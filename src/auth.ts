b17924b1af1ba9257cdaf875ad724343e6501ae7
npaik <nakhyunpaik@gmail.com>
2023-11-27 11:23:11 -0800

    Add localforage package and update next-auth version

--- src/auth.ts
+++ src/auth.ts
@@ -1,31 +1,33 @@
-import NextAuth, { NextAuthConfig } from "next-auth"
-import GitHub from "next-auth/providers/GitHub"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import { db } from "@/db"
+import NextAuth, { NextAuthConfig } from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db";
 
 export const authConfig = {
   providers: [GitHub],
   adapter: DrizzleAdapter(db),
   callbacks: {
     async session({ session, user }) {
-      session.user.id = user.id
-      return session
+      session.user.id = user.id;
+      return session;
     },
     authorized({ auth, request: { nextUrl } }) {
-      return true
-      const isLoggedIn = !!auth?.user
-      const paths = ["/"]
-      const isProtected = paths.some((path) => nextUrl.pathname.startsWith(path))
+      return true;
+      const isLoggedIn = !!auth?.user;
+      const paths = ["/"];
+      const isProtected = paths.some((path) =>
+        nextUrl.pathname.startsWith(path)
+      );
 
       if (isProtected && !isLoggedIn) {
-        const redirectUrl = new URL("api/auth/signin", nextUrl.origin)
-        redirectUrl.searchParams.append("callbackUrl", nextUrl.href)
-        return Response.redirect(redirectUrl)
+        const redirectUrl = new URL("api/auth/signin", nextUrl.origin);
+        redirectUrl.searchParams.append("callbackUrl", nextUrl.href);
+        return Response.redirect(redirectUrl);
       }
 
-      return true
+      return true;
     },
   },
-} satisfies NextAuthConfig
+} satisfies NextAuthConfig;
 
-export const { handlers, auth, signOut } = NextAuth(authConfig)
+export const { handlers, auth, signOut } = NextAuth(authConfig);
